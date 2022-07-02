5af326399ea5b064e2d709b490a984db699ffd24
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-07-02 09:32:25 -0300

    Quesito 5

--- src/Search.js
+++ src/Search.js
@@ -2,11 +2,45 @@ import React from 'react';
 import Header from './Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      disabledBol: true,
+    };
+    this.ableSearch = this.ableSearch.bind(this);
+  }
+
+  ableSearch({ target }) {
+    const { value } = target;
+    const min = 2;
+    if (value.length >= min) {
+      this.setState({ disabledBol: false });
+    } else {
+      this.setState({ disabledBol: true });
+    }
+  }
+
   render() {
+    const { disabledBol } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <h1>Search</h1>
+        <label htmlFor="search" className="search">
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            onChange={ this.ableSearch }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ disabledBol }
+        >
+          Pesquisar
+
+        </button>
       </div>
     );
   }
