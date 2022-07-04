297ae6c229a6d1137dd70de3d6fd49cf86c2ec7c
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-07-04 10:02:25 -0300

    Quesito 12 - Falha no último teste

--- src/Favorites.js
+++ src/Favorites.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import Loading from './Carregando';
 import Header from './Header';
+import { getFavoriteSongs } from './services/favoriteSongsAPI';
+import MusicCard from './MusicCard';
 
 class Favorites extends React.Component {
+  constructor() {
+    super();
+
+    this.state = ({
+      favSongs: [],
+      loading: false,
+    });
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const songsArray = await getFavoriteSongs();
+    this.setState({ favSongs: songsArray });
+    this.setState({ loading: false });
+  }
+
+  componentDidUpdate() {
+    console.log('oi');
+  }
+
   render() {
+    const { favSongs, loading } = this.state;
     return (
       <div data-testid="page-favorites">
         <Header />
-        <h1>Favorites</h1>
+        { loading ? (<Loading />)
+          : (favSongs.map((song, index) => (
+            <MusicCard value={ song } key={ index } />
+          ))) }
       </div>
     );
   }
