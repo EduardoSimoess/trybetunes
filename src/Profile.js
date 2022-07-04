d0e9ad4f427522f384db0f9ffee3ec65338b92ba
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-07-04 10:32:28 -0300

    Quesito 13, último teste do quesito 12 ainda falho

--- src/Profile.js
+++ src/Profile.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from './Header';
+import { getUser } from './services/userAPI';
+import Loading from './Carregando';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+
+    this.state = ({
+      loading: false,
+      usersData: '',
+    });
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const user = await getUser();
+    console.log(user);
+    this.setState({ usersData: user });
+    this.setState({ loading: false });
+  }
+
   render() {
+    const { loading, usersData } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
-        <h1>Profile</h1>
+        { loading ? (<Loading />) : (
+          <div>
+            <p>{usersData.name}</p>
+            <p>{usersData.email}</p>
+            <p>{usersData.description}</p>
+            <img src={ usersData.image } alt="Profile" data-testid="profile-image" />
+            <Link to="/profile/edit">Editar perfil</Link>
+          </div>
+        )}
       </div>
     );
   }
